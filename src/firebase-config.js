36748e61d6c8582dcc7c98b9f0dd4110970eb1b5
Olivier <exploit.diridf@gmail.com>
2024-12-07 18:58:03 +0100

    Adding Google Authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2a-8v1wlwytrzAFlDNP4Ag3bTrzw0AFo",
+    authDomain: "friendlychat-tou.firebaseapp.com",
+    projectId: "friendlychat-tou",
+    storageBucket: "friendlychat-tou.firebasestorage.app",
+    messagingSenderId: "431505372611",
+    appId: "1:431505372611:web:dcb8126e6a2e9c5198f976"
+  };
