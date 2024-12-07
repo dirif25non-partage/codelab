76bc5c6355dca7720789ddff1eb0fdbbefa95440
Olivier <exploit.diridf@gmail.com>
2024-12-07 20:26:50 +0100

    add message

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA2a-8v1wlwytrzAFlDNP4Ag3bTrzw0AFo",
-    authDomain: "friendlychat-tou.firebaseapp.com",
+    authDomain: "friendlychat-tou.firebaseapp.com", // https://codelab--friendlychat-tou.us-central1.hosted.app/
     projectId: "friendlychat-tou",
     storageBucket: "friendlychat-tou.firebasestorage.app",
     messagingSenderId: "431505372611",
